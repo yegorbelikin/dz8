d820065fe776738401bf48c992e9d12859bea463
Yegor <yegor.belikin@gmail.com>
2025-01-12 21:20:02 +0500

    Corrected FormDate

--- src/main/java/ru/netology/service/FormDate.java
+++ src/main/java/ru/netology/service/FormDate.java
@@ -1,7 +1,7 @@
 package ru.netology.service;
 
 public class FormDate {
-    public long day;
-    public long month;
-    public long year;
+    public int day;
+    public int month;
+    public int year;
 }
